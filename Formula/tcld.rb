5ddc139528acd003f36cc933acbd696e43650d9d
Travis <1060873+tminusplus@users.noreply.github.com>
2023-08-30 16:57:19 -0700

    Include version information in tcld builds and verify version in brew test (#16)

--- Formula/tcld.rb
+++ Formula/tcld.rb
@@ -1,23 +1,32 @@
 class Tcld < Formula
   desc "Temporal Cloud CLI (tcld)"
   homepage "https://temporal.io/"
-  url "https://github.com/temporalio/tcld/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "6a7c298cc7dbe7c025e1ddd06b493d41f338d403232d504d2cdde35e06c9d07e"
+  url "https://github.com/temporalio/tcld.git",
+    tag: "v0.13.0",
+    revision: "31a8f16fbf35094deee6af3cee2e74538055da84"
 
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"tcld", "./cmd/tcld/main.go", "./cmd/tcld/fx.go"
+    ldflags = %W[
+      -s -w
+      -X github.com/temporalio/tcld/app.version=v#{version}
+      -X github.com/temporalio/tcld/app.commit=#{Utils.git_short_head(length: 12)}
+      -X github.com/temporalio/tcld/app.date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"tcld", "./cmd/tcld"
   end
 
   test do
-    # Given tcld is pointless without a server, not much intersting to test here.
+    # Verify the version string of tcld is set correctly.
     run_output = shell_output("#{bin}/tcld version 2>&1")
-    assert_match "Version", run_output
+    assert_match "v#{version}", run_output
 
-    run_output = shell_output("#{bin}/tcld -s localhost:1234 n l 2>&1")
-    assert_match "rpc error", run_output
+    # Basic validation of help.
+    run_output = shell_output("#{bin}/tcld help")
+    assert_match "tcld", run_output
   end
 end
