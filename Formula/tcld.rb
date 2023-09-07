73f85e9b3d45ce6b57c12e4f183be8405a80606c
Travis <1060873+tminusplus@users.noreply.github.com>
2023-09-07 15:59:42 -0700

    Release tcld v0.14.1 (#17)

--- Formula/tcld.rb
+++ Formula/tcld.rb
@@ -2,8 +2,8 @@ class Tcld < Formula
   desc "Temporal Cloud CLI (tcld)"
   homepage "https://temporal.io/"
   url "https://github.com/temporalio/tcld.git",
-    tag: "v0.13.0",
-    revision: "31a8f16fbf35094deee6af3cee2e74538055da84"
+    tag: "v0.14.1",
+    revision: "49cf16b25027bfee2f7294ec81cd5c9e385cc309"
 
   license "MIT"
 
