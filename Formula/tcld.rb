68cae587a815cc4510f9cc83c3ae9dfc371c30e8
Jeri Lane <jeri.lane@temporal.io>
2026-09-01 12:39:24 -0700

    Move temporal-cloud from prerelease tap (#60)
    * Move temporal-cloud from prerelease tap
    
    * tcld formatting fixes for brew style
    
    * tcld: add 0.55.0 bottle.
    
    * temporal-cloud: update 0.0.4 bottle.

--- Formula/tcld.rb
+++ Formula/tcld.rb
@@ -2,10 +2,17 @@ class Tcld < Formula
   desc "Temporal Cloud CLI (tcld)"
   homepage "https://temporal.io/"
   url "https://github.com/temporalio/tcld.git",
-     tag: "v0.55.0",
+     tag:      "v0.55.0",
      revision: "4bde81d5c73d36a3316a73f21416cbb6af983384"
 
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/temporalio/homebrew-brew/releases/download/tcld-0.55.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "38219956735791aeeb017203a9fcab99228483ecab97da954622e21c020c35fe"
+    sha256 cellar: :any,                 x86_64_linux: "5055fc5f8e0958f56c6e8a131b12f2b5301a2ec21b79e063302bc62161d19a1e"
+  end
 
   depends_on "go" => :build
 
---
+++ Formula/temporal-cloud.rb
@@ -0,0 +1,34 @@
+class TemporalCloud < Formula
+  desc "Cloud plugin for the Temporal CLI (Pre-release)"
+  homepage "https://github.com/temporalio/cloud-cli"
+
+  url "https://github.com/temporalio/cloud-cli/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "32ca6328da291057f62863b45157000b41a96190837ae17df78a791620c5e5f3"
+  license "MIT"
+  head "https://github.com/temporalio/cloud-cli.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/temporalio/homebrew-brew/releases/download/temporal-cloud-0.0.4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "a4c75e0d7908a1f5129f3bf34319e842eccb61cb802616c5ee346397dd772bc1"
+    sha256 cellar: :any,                 x86_64_linux: "c4b68266badd4e935341abd7de4dcd036c132700df95e3d599a0775df9525969"
+  end
+
+  depends_on "go" => :build
+  depends_on "temporal"
+
+  def install
+    v = build.head? ? "0.0.0-HEAD+#{Utils.git_short_head}" : version.to_s
+    ldflags = "-s -w -X github.com/temporalio/cloud-cli/temporalcloudcli.Version=#{v}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/temporal-cloud"
+  end
+
+  test do
+    run_output = shell_output("#{bin}/temporal-cloud --version")
+    assert_match "cloud version #{version}", run_output
+  end
+end
